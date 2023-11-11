7090d0d82f90649da4d8889ebf4e44a525087b4d
Kokanov Alexandr <kokanov.av@students.dvfu.ru>
2023-11-12 07:54:13 +1000

    update show points

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from Point import Point
 from tests import *
 
+import matplotlib.lines as mlines
+import matplotlib.pyplot as plt
+
 endCoords = 10 ** 4
 
 def genaratePoints(countPoinst):
@@ -13,17 +16,51 @@ def genaratePoints(countPoinst):
 
 
 def showPoints(points):
-    fig = plt.figure()
+    clusters = getClusters(points)
+    largestClaster = max(clusters, key = len)
+    clusters.remove(largestClaster)
+
+    countRed = len(largestClaster)
+    countBlue = len(points) - countRed
+    colors = ["red"] * countRed + ["blue"] * countBlue
+
+    print(largestClaster)
+    fig = plt.figure()  
     ax = fig.add_subplot(111, projection='3d')
 
-    x = [point.x for point in points]
-    y = [point.y for point in points]
-    z = [point.z for point in points]
+    x = [points[i].x for i in largestClaster]
+    y = [points[i].y for i in largestClaster]
+    z = [points[i].z for i in largestClaster]
+    
+    for c in clusters:
+        for j in c:
+            x.append(points[j].x)
+            y.append(points[j].y)
+            z.append(points[j].z)
 
-    ax.scatter(x, y, z, c = "red", alpha=1.0)
+    coords = [0, 3000, 6000, 9000]
+    ax.set_xticks(coords)
+    ax.set_yticks(coords)
+    ax.set_zticks(coords)
+
+    redBall = mlines.Line2D([], [], color='red', marker='o', linestyle='None',
+                            markersize=5, label='Самый крупный кластер')
+    
+    blueBall = mlines.Line2D([], [], color='blue', marker='o', linestyle='None',
+                            markersize=5, label='Остальные точки')
+    
+
+    plt.legend(handles=[redBall, blueBall], bbox_to_anchor=(1.3, 1.1))
+
+    ax.scatter(x, y, z, c = colors, alpha=1.0)
+    ax.view_init(elev=40, azim=30)  # Например, повернуть по оси Z на 30 градусов
 
     plt.show()
 
+
+
+points = genaratePoints(50)
+showPoints(points)
 
 unittest.main()
 
